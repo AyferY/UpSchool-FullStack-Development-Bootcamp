f1204eec2e6f2fbcf9b312e1050debb2f06d26c9
Ayfer Yıldırım <69862941+AyferY@users.noreply.github.com>
2023-03-02 22:25:31 +0300

    Add files via upload

---
+++ Ödev1/Son/CreateRandomPassword.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Son
+{
+    public class CreateRandomPassword
+    {
+        public string chars { get; set; }
+        public int lengths { get; set; }
+
+        public string GetRandomPassword(int lengths)
+        {
+           
+
+            StringBuilder sb = new StringBuilder();
+            Random rnd = new Random();
+
+            for (int i = 0; i < lengths; i++)
+            {
+                int index = rnd.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Ödev1/Son/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using Son;
+
+Console.WriteLine("***********************************************");
+Console.WriteLine("Welcome to the B E S T P A S S W O R D M A N A G E R !");
+Console.WriteLine("***********************************************");
+Console.WriteLine("Do you want to include number?");
+
+string Char = String.Empty;
+if (Console.ReadLine().Contains("y") || Console.ReadLine().Contains("yes"))
+{
+    Char = "0123456789";
+}
+
+
+Console.WriteLine("OK! How about lowercase characters?");
+if (Console.ReadLine().Contains("y") || Console.ReadLine().Contains("yes"))
+{
+    Char = Char + "abcdefghijklmnopqrstuvwxyz";
+}
+
+
+Console.WriteLine("Very nice! How about uppercase characters?");
+if (Console.ReadLine().Contains("y") || Console.ReadLine().Contains("yes"))
+{
+    Char = Char + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+
+
+Console.WriteLine("All right! We are almost done. Would you also want to add special characters?");
+if (Console.ReadLine().Contains("y") || Console.ReadLine().Contains("yes"))
+{
+    Char = Char + "/*-+";
+}
+
+
+Console.WriteLine("Great! Lastly. How long do you want to keep your password length?");
+string PassLength = Console.ReadLine();
+
+
+CreateRandomPassword CRandomPassword = new CreateRandomPassword();
+
+CRandomPassword.chars = Char;
+
+
+Console.WriteLine($"New Password : {CRandomPassword.GetRandomPassword(Convert.ToInt32(PassLength))}");
+
+Console.ReadLine();
