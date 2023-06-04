7c61996a31caf285085a22912cf7cc3da6f205f1
Ayfer Yıldırım <ayferyildirim3482@gmail.com>
2023-06-04 21:36:09 +0300

    UpStorage

---
+++ FinalProject/UpStorage/UpStorage.Domain/Enum/ProductCrawlType.cs
@@ -0,0 +1,9 @@
+﻿namespace UpStorage.Domain.Enum
+{
+    public enum ProductCrawlType
+    {
+        All=0, //tümünün tipi
+        OnDiscount=1, //indirimli olanların tipi(üzerinde sale yazanlar bunlar)
+        NonDiscount=2//indirimsiz olanların tipi
+    }
+}
