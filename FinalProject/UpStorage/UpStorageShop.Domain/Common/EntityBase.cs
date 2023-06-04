6262f6496c60c3410550ba713a14b88bf752a087
Ayfer Yıldırım <ayferyildirim3482@gmail.com>
2023-06-04 21:02:34 +0300

    delete

--- FinalProject/UpStorage/UpStorageShop.Domain/Common/EntityBase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UpStorage.Domain.Common
-{
-    public class EntityBase
-    {
-        public Guid Id { get; set; }
-        public DateTimeOffset CreatedOn { get; set; }
-    }
-}
--- FinalProject/UpStorage/UpStorageShop.Domain/Common/Interfaces/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using UpStorage.Domain.Common.Models.Email;
-
-namespace UpStorage.Domain.Common.Interfaces
-{
-    public interface IEmailService
-    {
-        void SendEmailConfirmation(SendEmailConfirmationDto sendEmailConfirmationDto);
-    }
-}
